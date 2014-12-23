776fdb8c7b9477977072b614f37cc794c4dec12b
Jamie Nichols <jamie337nichols@gmail.com>
2014-12-22 22:13:42 -0600

    small change

--- bin/js/Main.js
+++ bin/js/Main.js
@@ -2,13 +2,8 @@ function init(){
 
 	"com.*".import();
 		
-	var test_ar = new Array("something "," something else"," another thing ");
-	//console.log(test_ar);
-	//console.log(test_ar.trim());
-	
-	
 	smile = new Smiley();
 	smile.mood = "happy";
-	//smile.Smile();
+	smile.Smile();
 	
 }
--- bin/js/classes/classes.js
+++
@@ -1,6 +0,0 @@
-(function(window){var GetClasses = function(){}
-GetClasses.prototype.classes = new Array(
-	"Jquery",
-	"Createjs",
-	"com.*"
-);window.GetClasses = GetClasses;})(window)
